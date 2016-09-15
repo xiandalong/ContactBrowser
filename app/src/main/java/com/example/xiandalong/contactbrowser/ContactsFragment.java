07fcd5de31abaac15064274582a4f71da4a02bdd
xiandalong <kelvinlong8897@gmail.com>
2016-09-15 14:26:59 -0400

    add SearchView in the action bar

--- app/src/main/java/com/example/xiandalong/contactbrowser/ContactsFragment.java
+++ app/src/main/java/com/example/xiandalong/contactbrowser/ContactsFragment.java
@@ -11,7 +11,12 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.LoaderManager;
 import android.support.v4.content.CursorLoader;
 import android.support.v4.content.Loader;
+import android.support.v4.view.MenuItemCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.SearchView;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
@@ -27,22 +32,26 @@ public class ContactsFragment extends Fragment {
 
     // Defines the id of the loader for later reference
     public static final int CONTACT_LOADER_ID = 78; // From docs: A unique identifier for this loader. Can be whatever you want.
+    public static final String QUERY_NAME = "nameQuery";
     private SimpleCursorAdapter adapter;
-    // Defines the asynchronous callback for the contacts data loader
     private LoaderManager.LoaderCallbacks<Cursor> contactsLoader =
             new LoaderManager.LoaderCallbacks<Cursor>() {
                 // Create and return the actual cursor loader for the contacts data
                 @Override
                 public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+                    String query = args.getString(QUERY_NAME);
                     // Define the columns to retrieve
-                    String[] projectionFields = new String[]{ContactsContract.Contacts._ID,
-                            ContactsContract.Contacts.DISPLAY_NAME,
-                            ContactsContract.CommonDataKinds.Phone.NUMBER};
+                    String[] projectionFields = new String[]{ ContactsContract.Contacts._ID,
+                                                              ContactsContract.Contacts.DISPLAY_NAME,
+                                                              ContactsContract.CommonDataKinds.Phone.NUMBER };
                     // Construct the loader
                     CursorLoader cursorLoader = new CursorLoader(getActivity(),
                                                                  ContactsContract.CommonDataKinds.Phone.CONTENT_URI,// URI
                                                                  projectionFields,// projection fields
-                                                                 null,// the selection criteria
+                                                                 query == null ? null :
+                                                                         ContactsContract.Contacts.DISPLAY_NAME +
+                                                                                 " like '%" + query + "%'",
+// the selection criteria
                                                                  null,// the selection args
                                                                  "DISPLAY_NAME ASC"
                                                                  // the sort order
@@ -67,15 +76,20 @@ public class ContactsFragment extends Fragment {
                     // Clear the Cursor we were using with another call to the swapCursor()
                     adapter.swapCursor(null);
                 }
+
             };
+
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
+        setHasOptionsMenu(true);
         // inflate rootView
         View rootView = inflater.inflate(R.layout.fragment_main, container, false);
         setupCursorAdapter();
+
+
         // Initialize the loader with a special ID and the defined callbacks from above
         getActivity().getSupportLoaderManager().initLoader(CONTACT_LOADER_ID,
                 new Bundle(), contactsLoader);
@@ -95,8 +109,44 @@ public class ContactsFragment extends Fragment {
         });
 
 
-
         return rootView;
+    }
+
+
+    @Override
+    public void onPrepareOptionsMenu(Menu menu) {
+
+
+        menu.clear();
+        getActivity().getMenuInflater().inflate(R.menu.search_option_menu, menu);
+        MenuItem item = menu.findItem(R.id.action_search);
+        SearchView searchView = new SearchView(((AppCompatActivity) getActivity()).getSupportActionBar()
+                                                       .getThemedContext());
+        MenuItemCompat.setShowAsAction(item,
+                                       MenuItemCompat.SHOW_AS_ACTION_COLLAPSE_ACTION_VIEW |
+                                               MenuItemCompat.SHOW_AS_ACTION_IF_ROOM);
+        MenuItemCompat.setActionView(item, searchView);
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                updateLoader(query);
+                return true;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                updateLoader(newText);
+                return true;
+            }
+
+            private void updateLoader(String query) {
+                Bundle args = new Bundle();
+                args.putString(QUERY_NAME, query);
+                getLoaderManager().restartLoader(CONTACT_LOADER_ID, args, contactsLoader);
+            }
+        });
+
+
     }
 
     // Create simple cursor adapter to connect the cursor dataset we load with a ListView
@@ -113,6 +163,8 @@ public class ContactsFragment extends Fragment {
                 getActivity(), R.layout.contact_list_item,
                 null, uiBindFrom, uiBindTo,
                 0);
+
+
     }
 
 
